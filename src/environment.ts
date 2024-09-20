a8feba0ef1162d71cd041a8ef1dde586ce92bafa
Fernando Escobar <57463087+FerEscobarDev@users.noreply.github.com>
2024-09-19 20:57:08 -0500

    exposición docker file

--- src/environment.ts
+++ src/environment.ts
@@ -1,9 +1,9 @@
 
 const esDesarrollo: boolean = location.hostname === 'localhost' || location.hostname === '127.0.0.1';
 
-const urlBaseApi: string = esDesarrollo ? 'http://localhost' : 'https://receramica.com';
+const urlBaseApi: string = esDesarrollo ? 'http://localhost' : 'https://ricardo-admin.receramica.com';
 
-const authToken: string = esDesarrollo ? `Bearer YKgCrV95nc1AOL66hYUzWBSyC3YhVhU8SwLTm13A3b0eaf5e` : 'Bearer YKgCrV95nc1AOL66hYUzWBSyC3YhVhU8SwLTm13A3b0eaf5e';
+const authToken: string = esDesarrollo ? `Bearer YKgCrV95nc1AOL66hYUzWBSyC3YhVhU8SwLTm13A3b0eaf5e` : 'Bearer ojstPtWPNm7nguTDiyqSY7dMopSQOlLB6GvgPSCf087d6484';
 
 
 
